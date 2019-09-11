c504995461ab736536309a0898ed80ed6f5a6d44
Ben Memberg <benmemberg@gmail.com>
2019-09-11 16:23:20 -0400

    Remove chatbot plugin

--- site/common/views.py
+++ site/common/views.py
@@ -3,17 +3,6 @@ from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-from chatterbot import ChatBot
-
-chatbot = ChatBot(
-    'Ron Obvious',
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
-)
-
-# Train based on the english corpus
-
-#Already trained and it's supposed to be persistent
-#chatbot.train("chatterbot.corpus.english")
 
 @csrf_exempt
 def get_response(request):
@@ -23,7 +12,8 @@ def get_response(request):
 		data = json.loads(request.body)
 		message = data['message']
 
-		chat_response = chatbot.get_response(message).text
+		# chat_response = chatbot.get_response(message).text
+		chat_response = "Chatbot not home, you said: " + message
 		response['message'] = {'text': chat_response, 'user': False, 'chat_bot': True}
 		response['status'] = 'ok'
 
@@ -37,8 +27,5 @@ def get_response(request):
 
 
 def home(request, template_name="home.html"):
-	context = {
-			'title': 'Chatbot Version 1.0',
-			'shops': ['buckeye donuts', 'mcdonalds']
-			}
+	context = {'title': 'Chatbot Version 1.0'}
 	return render_to_response(template_name, context)
